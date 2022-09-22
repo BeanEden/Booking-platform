e9fd436e153b121ba50ffea25531a1ed9a6ac226
JC <bean3den@gmail.com>
2022-09-22 09:38:47 +0200

    Solving the issue (try except in showSummary) Adding tests (pytest and locust) and managing db

--- tests/performance_tests/locustfile.py
+++ tests/performance_tests/locustfile.py
@@ -2,9 +2,6 @@ from locust import HttpUser, task
 
 
 valid_email = "admin@irontemple.com"
-club = "Simply Lift"
-competition = "Spring Festival"
-places_bought = 2
 
 
 class ProjectPerfTest(HttpUser):
@@ -15,19 +12,8 @@ class ProjectPerfTest(HttpUser):
 
     @task(6)
     def showSummary(self):
-        self.client.post('showSummary', data={'email': [valid_email]})
+        response = self.client.post('showSummary', data={'email': [valid_email]})
 
     @task(6)
     def logout(self):
         self.client.get("logout")
-
-    @task(6)
-    def clubsTable(self):
-        self.client.get("clubs")
-
-    @task(6)
-    def purchasePlace(self):
-        self.client.post('purchasePlaces',
-                         data=dict(club=club,
-                                   competition=competition,
-                                   places=places_bought))
