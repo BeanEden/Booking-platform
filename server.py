2c61513be390137025255ab5c6f8bfc73a468bbd
JC <bean3den@gmail.com>
2022-09-22 10:19:39 +0200

    Addong the feature (new function and new template) Adding tests (pytest and locust) and managing db

--- server.py
+++ server.py
@@ -1,15 +1,16 @@
 import json
 from flask import Flask,render_template,request,redirect,flash,url_for
+import os
 
 
 def loadClubs():
-    with open('clubs.json') as c:
+    with open(os.getcwd()+'/database/clubs.json') as c:
          listOfClubs = json.load(c)['clubs']
          return listOfClubs
 
 
 def loadCompetitions():
-    with open('competitions.json') as comps:
+    with open(os.getcwd()+'/database/competitions.json') as comps:
          listOfCompetitions = json.load(comps)['competitions']
          return listOfCompetitions
 
@@ -20,14 +21,19 @@ app.secret_key = 'something_special'
 competitions = loadCompetitions()
 clubs = loadClubs()
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/showSummary',methods=['POST'])
 def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html',club=club,competitions=competitions)
+    try:
+        club = [club for club in clubs if club['email'] == request.form['email']][0]
+    except IndexError:
+        return index(error_message="Sorry, that email wasn't found.")
+    return render_template('welcome.html', club=club, competitions=competitions)
 
 
 @app.route('/book/<competition>/<club>')
@@ -41,7 +47,7 @@ def book(competition,club):
         return render_template('welcome.html', club=club, competitions=competitions)
 
 
-@app.route('/purchasePlaces',methods=['POST'])
+@app.route('/purchasePlaces', methods=['POST'])
 def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
@@ -52,6 +58,10 @@ def purchasePlaces():
 
 
 # TODO: Add route for points display
+@app.route('/clubs')
+def clubsTable():
+    sorted_list = sorted(clubs, key=lambda item: item['name'])
+    return render_template('clubs.html', clubs=sorted_list)
 
 
 @app.route('/logout')
